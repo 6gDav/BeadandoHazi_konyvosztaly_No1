75aed15f1fc78bf6987717f05cd9bd11396dcb05
Dávid Török <torok.david@diak.remenyikzs.hu>
2024-10-22 15:59:24 +0200

    Add project files.

---
+++ BeadandoHazi_konyvosztaly_No1/Konyv.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+
+namespace BeadandoHazi_konyvosztaly_No1
+{
+    internal class Konyv
+    {
+        private string _cim { get; set; }
+        private string _szerzo { get; set; }
+        private int _oldalszam { get; set; }
+        private double _ar { get; set; }
+
+        public string Cim
+        { 
+            get => _cim;
+            set => _cim = value;
+        }
+
+        public string Szerzo
+        { 
+            get => _szerzo;
+            set => _szerzo = value;
+        }
+
+        public int Oldalszam
+        {
+            get => _oldalszam;
+            set => _oldalszam = value;
+        }
+
+        public double Ar
+        {
+            get => _ar;
+            set => _ar = value;
+        }
+
+        public Konyv(string cim, string szerzo, int oldalszam, double ar)
+        {
+            this.Cim = cim;
+            this.Szerzo = szerzo;
+            this.Oldalszam = oldalszam;
+            this.Ar = ar;
+        }
+
+        public Konyv(string cim, string szerzo, int oldalszam) : this(cim, szerzo, oldalszam, 3_000)
+        {
+
+        }
+
+        public override string ToString()
+        {
+            return $"Cim: {this.Cim}, Szezo: {this.Szerzo}, Oldalszam: {this.Oldalszam}, Ar: {this.Ar}";
+        }
+
+        public void Vastag_e()
+        {
+            if (this.Oldalszam > 500) Console.WriteLine("Vastag");
+        }
+
+        public void Draga_e() 
+        {
+            if (this.Ar > 5_000) Console.WriteLine("Draga");
+        }
+    }
+}
---
+++ BeadandoHazi_konyvosztaly_No1/Program.cs
@@ -0,0 +1,67 @@
+﻿namespace BeadandoHazi_konyvosztaly_No1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n_szam = AdatBK<int>("Adja meg hogy hany konyvet szeretne");
+            Console.WriteLine(new string('|', 25));
+            Konyv[] konyvek = new Konyv[n_szam];
+
+            for (int i = 0; i < konyvek.Length; i++)
+            {
+                string cim = AdatBK<string>((i + 1).ToString() + " Adja meg a cim konyvet.");
+                string szerzo = AdatBK<string>((i + 1).ToString() + " Adja meg a konyv szerzojet.");
+                int oldalszam = AdatBK<int>((i + 1).ToString() + " Adja meg a konyv oldalszamat.");
+                double ar = AdatBK<double>((i + 1).ToString() + " Adja meg a konyv arat.");
+
+                if (ar != 0)
+                {
+                    konyvek[i] = new Konyv(cim, szerzo, oldalszam, ar);
+                }
+                else
+                {
+                    konyvek[i] = new Konyv(cim, szerzo, oldalszam);
+                }
+                Console.WriteLine(new string('*', 25));
+            }
+
+            Console.WriteLine(new string('|', 25));
+
+            int iter = 1;
+            foreach (Konyv item in konyvek)
+            {
+                Console.WriteLine(iter.ToString() + " " + item.ToString());
+                item.Vastag_e();
+                item.Draga_e();
+            }
+
+            Console.ReadKey();
+        }
+
+        private static T AdatBK<T>(string adatbksov)
+        {
+            while (true)
+            {
+                try
+                {
+                    Console.Write(adatbksov + " ");
+                    string bekertadat = Console.ReadLine();
+
+                    if (string.IsNullOrEmpty(bekertadat) && typeof(T) == typeof(double))
+                    {
+                        //return (T)Convert.ChangeType(3_000, typeof(T));
+                        return default(T);
+                    }
+
+                    return (T)Convert.ChangeType(bekertadat, typeof(T));
+                }
+                catch (Exception er)
+                {
+                    Console.WriteLine("Hiba a megadott adatban! ....");
+                    Console.WriteLine(er.Message);
+                }
+            }
+        }
+    }
+}
